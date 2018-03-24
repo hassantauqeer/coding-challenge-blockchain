76cf295a5b37911b883be01dfe682ee259d9b63c
Hassan Khokhar <tauqeerhassan66@gmail.com>
2018-03-24 09:21:52 +0500

    updated token contract address

--- wallet-dApp/token/factory.js
+++ wallet-dApp/token/factory.js
@@ -3,7 +3,7 @@ import SavingToken from './build/SavingToken.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(SavingToken.interface),
-  '0xd26604e4EDB2307EA36A3f7BE95e3f213aB15004'
+  '0x91bbb106f68f4f87dE8789E9B0423A6CCb1E68A6'
 );
 
 export default instance;
--- wallet-dApp/token/saveToken.js
+++ wallet-dApp/token/saveToken.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 import * as SavingToken from './build/SavingToken.json';
 
-export default new web3.eth.Contract(JSON.parse(SavingToken.interface), "0xd26604e4EDB2307EA36A3f7BE95e3f213aB15004");
+export default new web3.eth.Contract(JSON.parse(SavingToken.interface), "0x91bbb106f68f4f87dE8789E9B0423A6CCb1E68A6");
 
 // console.log(instance, web3, 'savetoken.js')
